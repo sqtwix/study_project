491329776cf7714beb342dba87470dcb24c22645
sqtwix <ivan20140767@gmail.com>
2026-02-26 00:39:51 +0300

    feat: loop

--- list_function.py
+++ list_function.py
@@ -1,3 +1,10 @@
 def create_list(n):
-    """Создает список от 0 до n-1"""
-    pass
+    """Создает список от 0 до n-1 циклом"""
+    result = []
+    for i in range(n):
+	result.append(i)
+    return result
+
+if __name__ == "__main__":
+    result = create_list(10)
+    print(result)
