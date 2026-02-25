f694f3744edcd4f40bb1e6710d9e0d18ecb8a8d7
sqtwix <ivan20140767@gmail.com>
2026-02-26 00:21:47 +0300

    feat: create_list function with realization

--- list_function.py
+++ list_function.py
@@ -1,3 +1,7 @@
 def create_list(n):
-    """Создает список от 0 до n-1"""
-    pass
+    """Создает список от 0 до n-1 используя генератор"""
+    return [i for i in range(n)]
+
+if __name__ == "__main__":
+    result = create_list(10)
+    print(result)
